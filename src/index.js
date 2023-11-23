5b3b2b88563834d4d8ccde06bcc4cf8fc8b57c5f
Jozef Steinhübl (xHyroM) <xhyrom.hyro@gmail.com>
2023-11-23 18:41:57 +0100

    refactor: make the message better

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
 import "dotenv/config";
 import { Client } from "discord.js";
 import { IntentsBitField } from "discord.js";
-import moment from 'moment-timezone';
+import moment from "moment-timezone";
 import cron from "node-cron";
 
 const CHANNEL_ID = "1162052161987940453";
 
 const client = new Client({
-  intents: [ IntentsBitField.Flags.Guilds ] 
+  intents: [IntentsBitField.Flags.Guilds],
 });
 
 client.on("ready", () => {
   console.log("I am ready!");
 
   cron.schedule("0 0 * * *", calculate, {
-    timezone: "Europe/London"
-  });
-  cron.schedule("0 12 * * *", calculate, {
-    timezone: "Europe/London"
+    timezone: "Europe/London",
   });
 });
 
@@ -26,13 +23,20 @@ const calculate = () => {
   const now = moment().tz("Europe/London");
   const start = moment().tz("Europe/London").startOf("year");
   const end = moment().tz("Europe/London").endOf("year");
-  const percentage = now.diff(start, "days") / end.diff(start, "days") * 100;
+  const percentage = (now.diff(start, "days") / end.diff(start, "days")) * 100;
 
   const elapsedDays = now.diff(start, "days");
   const remainingDays = end.diff(now, "days");
 
   // send message to channel
-  client.channels.cache.get(CHANNEL_ID).send(`The year is **${percentage}%** complete. (**${elapsedDays}** days elapsed, **${remainingDays}** days remaining)`);
+  client.channels.cache
+    .get(CHANNEL_ID)
+    .send(
+      [
+        `The year is now **${percentage}%** complete.`,
+        `We've had **${elapsedDays}** days so far, with **${remainingDays}** days left.`,
+      ].join("\n")
+    );
 };
 
 process.on("uncaughtException", console.log);
